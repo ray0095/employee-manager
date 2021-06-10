419e37baac91d737960be4a42165f5223a16c919
Rachel Strong <rachellynnstrong@gmail.com>
2021-06-10 19:01:57 -0400

    Adding basic file structure

---
+++ employeeee/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//Import pages
+//import components
+
+function App() {
+    return (
+        <Router>
+           
+
+        </Router>
+    )
+}
