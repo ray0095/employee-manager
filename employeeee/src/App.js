f0d77cb2d210e87eb182cc842026a4d10c54f547
Rachel Strong <rachellynnstrong@gmail.com>
2021-06-16 21:54:46 -0400

    adding main page, basic components, API util file

--- employeeee/src/App.js
+++ employeeee/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //Import pages
+import Home from "./pages/Home";
 //import components
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
 function App() {
     return (
         <Router>
-           
+           <div>
+               <Navbar />
+               <Wrapper>
+               <Route exact path= "/" component={Home} />
+               </Wrapper>
+           </div>
 
         </Router>
     )
 }
+
+export default App;
---
+++ employeeee/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import components
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Testing this out!</h1>
+        </div>
+    )
+}
+
+export default Home;
